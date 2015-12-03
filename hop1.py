a772736d67a929dff93ddecccbc44f4b0c57be5b
Dewey165 <agi2211@gmail.com>
2015-12-03 18:09:01 +0000

    lagfaering

--- hop1.py
+++ hop1.py
@@ -1,15 +1,14 @@
+
+
+
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec  3 12:20:52 2015
 
 @author: Ágúst
 """
-
-import csv as csv
 import numpy as np
 import pandas as pd
-
-csv_file_object = csv.reader(open('Innflutningur-hljodvarp-vs-sjonvarp.csv'))
 
 settings = {}
 
@@ -27,10 +26,18 @@ UnitName = [i.split(' ', 1)[1].strip() for i in data.Type]
 data['UnitNr.'] = pd.Series(UnitNumber, index=data.index)
 data['Type'] = pd.Series(UnitName, index=data.index)
 Flokkur85 = data[data['UnitNr.'] == 85]
+
 #print(Flokkur85)
 T1Sjonvorp = data[data['Type'] == 'Sjónvarpstæki']
+T1Sjonvorp.set_index('Year', inplace=True)
+#DeltaUnits is the difference between years in TotalUnits imported
+T1Sjonvorp.insert(2,'DeltaUnits', T1Sjonvorp['TotalUnits'].diff())
+
 
 T2Hljodvarpstaeki = data[data['Type'] == 'Hljóðvarpstæki']
+T2Hljodvarpstaeki.set_index('Year', inplace=True)
+T2Hljodvarpstaeki.insert(2,'DeltaUnits', T2Hljodvarpstaeki['TotalUnits'].diff())
+
 #print table 1 and 2
 print(T1Sjonvorp)
 print(T2Hljodvarpstaeki)
@@ -47,3 +54,5 @@ T3['SjonTotal%'] = 100*np.round((T3['TotalSjónvörp'] / T3['TotalUnits']), 3)
 T3['HljodTotal%'] = 100*np.round((T3['TotalHljóðvarpstæki'] / T3['TotalUnits']),3)
 T3['Leading Import'] = np.where(T3['SjonTotal%'] > T3['HljodTotal%'], 'Sjónvarpstæki', "Hljóðvarpstæki")
 #print(T3)
+T3.to_csv('Table3.csv')
+T4 = pd.DataFrame(index=['MaxYear', 'MaxUnits', 'MinYear', 'MinUnits','Average','1999vs2015','1999vs2015%'], columns=['Sjónvarpstæki','Hljóðvarpstæki'])
