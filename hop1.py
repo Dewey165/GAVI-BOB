44010e994bc968aeece8488874a9dea1b48f3f9b
Dewey165 <agi2211@gmail.com>
2015-12-03 16:30:05 +0000

    Tafla 3 kominn

--- hop1.py
+++ hop1.py
@@ -19,16 +19,31 @@ settings['sep'] = ';'
 data = pd.read_csv('import.csv', **settings)
 
 data.columns = ['Year', 'Type', 'TotalUnits', 'TotalTons', 'TotalCost']
-UnitNumber = [int(i.split(' ', 1)[0].strip(',')) for i in data.Type]
-UnitName = [i.split(' ', 1)[1] for i in data.Type]
+UnitNumber = [int(i.split(' ', 1)[0].strip(',').strip()) for i in data.Type]
+UnitName = [i.split(' ', 1)[1].strip() for i in data.Type]
 #print(UnitName)
 #vantar að laga þegar vörutegundir eru með mörg vörunúmer, t.d. kornvörur
 #forlykkja eins og UnitNumber og appenda það við UnitNr. og nota .isdigit()
 data['UnitNr.'] = pd.Series(UnitNumber, index=data.index)
 data['Type'] = pd.Series(UnitName, index=data.index)
 Flokkur85 = data[data['UnitNr.'] == 85]
-print(Flokkur85)
-T1Sjonvorp = data[data['Type'] == ' Sjónvarpstæki'].strip()
-T2Hljodvarpstaeki = data[data['Type'] == ' Hljóðvarpstæki']
+#print(Flokkur85)
+T1Sjonvorp = data[data['Type'] == 'Sjónvarpstæki']
+
+T2Hljodvarpstaeki = data[data['Type'] == 'Hljóðvarpstæki']
+#print table 1 and 2
 print(T1Sjonvorp)
 print(T2Hljodvarpstaeki)
+
+
+#Table 3 for Hljómvarpstæki
+index = pd.Series(i for i in T1Sjonvorp.Year)
+columns = ['TotalSjónvörp', 'TotalHljóðvarpstæki', 'TotalUnits', 'SjonTotal%', 'HljodTotal%', 'Leading Import']
+T3 = pd.DataFrame(index=index, columns=columns)
+T3['TotalSjónvörp'] = pd.Series([i for i in T1Sjonvorp.TotalUnits], index=T3.index)
+T3['TotalHljóðvarpstæki'] = pd.Series([i for i in T2Hljodvarpstaeki.TotalUnits], index=T3.index)
+T3['TotalUnits'] = T3['TotalSjónvörp'] + T3['TotalHljóðvarpstæki']
+T3['SjonTotal%'] = 100*np.round((T3['TotalSjónvörp'] / T3['TotalUnits']), 3)
+T3['HljodTotal%'] = 100*np.round((T3['TotalHljóðvarpstæki'] / T3['TotalUnits']),3)
+T3['Leading Import'] = np.where(T3['SjonTotal%'] > T3['HljodTotal%'], 'Sjónvarpstæki', "Hljóðvarpstæki")
+#print(T3)
