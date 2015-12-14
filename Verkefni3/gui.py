9f8b12c0b903d0a2d768e60276dd6fe228cc3a58
Gudmundur Hakon <gudmundurh14@ru.is>
2015-12-14 14:12:03 +0000

    added gui

---
+++ Verkefni3/gui.py
@@ -0,0 +1,13 @@
+gui.py
+print(catagory_id,name)
+type = input('Sláðu inn "1" ef þú vilt skoða eina skrá, sláðu inn 2 ef þú vilt bera saman skrár: ') 
+file_id = input('Sláðu inn númer á því sem þú vilt skoða: ')
+
+if type = 1
+#eitthvað merkilegt gerist
+
+elif type = 2
+print('This is great')
+file_id2= input('Sláðu inn númer á því sem þú vilt bera saman við') 
+else:
+print('Vesen')
